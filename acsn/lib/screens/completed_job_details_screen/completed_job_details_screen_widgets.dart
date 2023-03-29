c6dc29ed6e4f15ac2e10df239e64ded8ec15d77c
rushit5560 <rushit@omtecweb.com>
2023-03-29 17:54:44 +0530

    Update completed_job_details_screen_widgets.dart

--- acsn/lib/screens/completed_job_details_screen/completed_job_details_screen_widgets.dart
+++ acsn/lib/screens/completed_job_details_screen/completed_job_details_screen_widgets.dart
@@ -2,12 +2,21 @@ import 'package:acsn/constance/color.dart';
 import 'package:acsn/constance/extension.dart';
 import 'package:flutter/material.dart';
 
-
 class ListTileModule extends StatelessWidget {
   final String title;
   final String value;
+  final bool iconShow;
+  final Widget? leadingIcon;
 
-  const ListTileModule({Key? key, required this.title, required this.value}) : super(key: key);
+  // final Icon icon;
+
+  const ListTileModule(
+      {Key? key,
+      required this.title,
+      required this.value,
+      this.iconShow = false,
+      this.leadingIcon})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +37,7 @@ class ListTileModule extends StatelessWidget {
                   ),
                 ),
               ),
-
-              Text(
+              const Text(
                 ":",
                 style: TextStyle(
                   fontSize: 16,
@@ -41,15 +49,34 @@ class ListTileModule extends StatelessWidget {
           ),
         ),
         Expanded(
-          child: Text(
-            " $value",
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
+          child: iconShow
+              ? RichText(
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  text: TextSpan(
+                    text: " ",
+                    children: [
+                      WidgetSpan(child: leadingIcon!),
+                      TextSpan(
+                        text: " $value",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : Text(
+                  " $value",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
         ),
       ],
     ).commonSymmetricPadding(vertical: 5);
